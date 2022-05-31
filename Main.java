c21f238b61e32b0a1f7ace8ba62656a8e2e7462b
AP CODEZ <104022036+APCODEZ@users.noreply.github.com>
2022-05-31 23:04:22 +0000

    mega addition of lots and lots of code

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+
+import java.util.Scanner;  // Import the Scanner class
 public class Main {
   public static void main(String[] args) {
     String name = "john";
@@ -10,6 +12,25 @@ public class Main {
     System.out.println("After myNum changed to 20");
     System.out.println(myNum);
     System.out.println(myNum + 5);
+    boolean bVal = true;
+    System.out.println(bVal);
+    int x = 5, y=1, z=3;
+    System.out.println(x+y+z);
+    java.lang.Math.tan(52);
+    int time = 15;
+    String result = (time < 18) ? "Good day." : "Goodevening.";
+    System.out.println(result);
+    int[] num = {5, 0, 6};
+    System.out.println(num[2]);
+    
+    //using scanner class to get input statements
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter username");
+
+    int userName = myObj.nextInt();  // Read user input
+    System.out.println(userName + 5);  // Output user input
+    
+    
   }  
   
 }
